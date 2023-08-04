40821dccfcaf4befdb044703cf4e4227e4f67f0e
ruslan-byte <rbaroev0@gmail.com>
2023-08-04 15:21:55 +0300

    add logo

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,20 @@
   <div>
     Новая страница
     <el-button type="primary">res</el-button>
+    <br/>
+    <logo-icon class="logo"/>
   </div>
 </template>
 <script>
+import logo from "@/components/icons/logo.svg"
   export default {
+  	components:
+  	{
+  		'logo-icon': logo
+  	}
   }
 </script>
 
 <style>
-
+.logo{width: 124px;}
 </style>
--- vite.config.js
+++ vite.config.js
@@ -6,11 +6,13 @@ import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
-
+// svg-loader https://www.npmjs.com/package/vite-svg-loader
+import svgLoader from 'vite-svg-loader'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    svgLoader(),
     AutoImport({
       resolvers: [ElementPlusResolver()],
     }),
